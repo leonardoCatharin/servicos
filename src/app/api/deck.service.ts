ed47c9377897b5bde82a33ca482628ff0a5e8358
leonardoCatharin <leo_catharin@hotmail.com>
2022-02-18 14:08:26 -0300

    adicionado exercicio deck

--- src/app/api/deck.service.ts
+++ src/app/api/deck.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,37 @@ export class DeckService {
   /* api de baralhos */
   public host = 'http://deckofcardsapi.com/api/deck/';
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /* embaralha as cartas e recupera um novo deck */
+  public shuffle(count: number){
+    let pathShuffle = 'new/shuffle/?deck_count=' + count;
+
+    return new Promise((ret) => {
+
+      this.http.get(this.host + pathShuffle).subscribe(deck => {
+
+        ret(deck);
+
+      });
+    })
+  }
+
+  /* retira uma carta do deck */
+  public drawOneCard(deckId: string){
+    let pathDrawcard = deckId + '/draw/?count=1';
+
+    return new Promise((ret) => {
+
+      this.http.get(this.host + pathDrawcard).subscribe((deck:any) => {
+        if(deck && deck.cards){
+          ret(deck.cards);
+
+        } else {
+          ret([]);
+          
+        }
+      });
+    })
+  } 
 }
--- src/app/decks/decks.page.ts
+++ src/app/decks/decks.page.ts
@@ -1,3 +1,4 @@
+import { DeckService } from './../api/deck.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./decks.page.scss'],
 })
 export class DecksPage implements OnInit {
+  private countDeck = 1; /* variável utilizada para informar a quantidade de decks que se quer jogar */
+  private deck: any;     /* deck corrente */
+  
+  public cards = [];
 
-  constructor() { }
+  constructor(private ds: DeckService) { }
 
   ngOnInit() {
   }
 
+  /* inicia jogo */
+  initDeck(){
+    this.ds.shuffle(1).then(deck => {
+      this.cards = []; /* limpa a lista de cartas */
+      this.deck = deck;
+
+      alert('O jogo começou!');
+
+    });
+  }
+
+  /* retira uma carta e insere na lista de cartas */
+  public drawOneCard(){
+    if(!this.deck.deck_id){
+      alert('Nenhum deck foi inicializado!');
+
+    } else {
+      this.ds.drawOneCard(this.deck.deck_id).then(card => {
+        // card vem como Array, por isso [0]
+        this.cards.push(card[0]);
+        
+      });
+    }
+  }
 }
